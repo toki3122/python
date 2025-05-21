a032ee6e9e661a033fe04ea21601b2113953c259
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-05-21 23:16:47 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,2 @@
+print("hello")
+print("it's really good")
---
+++ variables.py
@@ -0,0 +1,20 @@
+name="toki"
+print("hello "+name)
+print(type(name)) #type() enables printing the variable type
+first_name="toki"
+last_name="3122"
+full_name= first_name + last_name
+print(full_name)
+age=21
+age+=1
+print(age)
+print(type(age))
+#concatenation of different data types is not possible in python
+print("your age is: "+str(age))
+#print("your age is: "+age) -> this will not work
+height = 250.5
+print(type(height))
+print("your height is: "+str(height)+"cm")
+human=False
+print(human)
+print(type(human))
