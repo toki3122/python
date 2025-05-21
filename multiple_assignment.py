24ff19cf1f2664b4fd85294f46d673bdb35adea2
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-05-21 23:51:58 +0600

    Add files via upload

---
+++ multiple_assignment.py
@@ -0,0 +1,13 @@
+#allows us to assign multiple variables at once
+#name="toki"
+#age=21
+#attractive=False
+name, age, attractive="toki", 21, False
+print(name)
+print(age)
+print(attractive)
+spongebob=patrick=sandy=squidward=30
+print(spongebob)
+print(patrick)
+print(sandy)
+print(squidward)
