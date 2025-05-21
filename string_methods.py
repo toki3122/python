926aee4a2b4fcd26b264985a6825579f5f14258c
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-05-22 00:18:48 +0600

    Add files via upload

---
+++ string_methods.py
@@ -0,0 +1,11 @@
+name="toki3122"
+print(len(name))
+print(name.find("i"))#to find the pos of some substring
+print(name.capitalize())#capitalizes the first alphabet
+print(name.upper())#turns the string upper case
+print(name.lower())#turns the string lower case
+print(name.isdigit())#to check if the string has only digit
+print(name.isalpha())#to check if the string has only alphabets
+print(name.count("o"))#counts the number of some string
+print(name.replace("o","a"))#replaces some substring
+print(name*3)
---
+++ type_casting.py
@@ -0,0 +1,10 @@
+#is used to change the data type of a variable
+x=1
+y=2.71
+z="3"
+y=int(y)
+print(x)
+print(y)#or typecast here so that the value will not change in the main variable
+print(z)
+print(z*3)
+print(int(z)*3)
