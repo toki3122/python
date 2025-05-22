b1172558f620c08b8400786d2c25503f5dd343c9
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-05-23 01:07:12 +0600

    Add files via upload

---
+++ math_func.py
@@ -0,0 +1,14 @@
+import math
+pi=3.14
+le=-3.14
+x=1
+y=2
+z=3
+print(round(pi))#rounds the value
+print(math.ceil(pi))#rounds up the value
+print(math.floor(pi))#rounds down the value
+print(abs(le))#absolute value
+print(pow(pi,2))
+print(math.sqrt(pi))
+print(max(x,y,z))
+print(min(x,y,z))
---
+++ user_input.py
@@ -0,0 +1,7 @@
+name=input("what is your name?: ")
+age=int(input("how old are you?: "))#to specify the the data type
+height=float(input("how tall are you?: "))
+print("hello "+name)
+#age=age+1
+print("you are "+str(age)+" years old")
+print("you are "+str(height)+" cm tall")
