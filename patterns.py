276c5194fbe7c55813b72b1e7a218285d3d1f7d1
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-06-18 15:25:03 +0600

    Update patterns.py

--- patterns.py
+++ patterns.py
@@ -54,3 +54,13 @@ for i in range(n,0,-1):
     for j in range(0,i):
         print("*",end="")
     print()
+#big triangle:
+n=int(input("enter n: "))
+for i in range(0,n):
+    for j in range(i+1):
+        print("*",end="")
+    print()
+for i in range(n,0,-1):
+    for j in range(1,i):
+        print("*",end="")
+    print()
