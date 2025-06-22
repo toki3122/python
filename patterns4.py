c8ad82d1a3fe91950323b060658df21fc2909541
toki3122 <160049158+toki3122@users.noreply.github.com>
2025-06-22 14:43:21 +0600

    Add files via upload

--- patterns4.py
+++ patterns4.py
@@ -1,44 +1,54 @@
 import math
-#floyd's triangle:
-n=int(input("enter n: "))
-m=1
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(m,end=" ")
-        m+=1
-    print()
-#pascal's triangle-1:
-n=int(input("enter n: "))
-for i in range(n):
-    for j in range(1,n-i+1):
-        print(" ",end="")
-    print(pow(11,i),end="")
-    print()
-#pascal's triangle-2:
+#void diamond:
 n=int(input("enter n: "))
 for i in range(1,n+1):
-    m=1
-    for j in range(1,n-i+1):
-        print(" ",end="")
-    for j in range(1,i+1):
-        print(m,end=" ")
-        m=int(m*(i-j)/j)
-    print()
-#heart:
-n=int(input("enter n: "))
-for i in range(math.floor(n/2),n+1,2):
-    for j in range(n-i-1,0,-1):
-        print(" ",end="")
-    for j in range(1,i+1):
-        print("*",end="")
-    for j in range(n-i,0,-1):
-        print(" ",end="")
-    for j in range(1,i+1):
-        print("*",end="")
-    print()
-for i in range(n,0,-1):
     for j in range(1,n-i+1):
         print(" ",end="")
     for j in range(1,2*i):
-        print("*",end="")
+        if j==1 or j==2*i-1:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+for i in range(n-1,0,-1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range(1,2*i):
+        if j==1 or j==2*i-1:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+#void triangle:
+n=int(input("enter n: "))
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range(1,2*i):
+        if j==1 or j==2*i-1 or i==1 or i==n:
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+#cross:
+n=int(input("enter n: "))
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if i==math.ceil(n/2) or j==math.ceil(n/2):
+            print("*",end="")
+        else:
+            print(" ",end="")
+    print()
+#number void tiangle:
+n=int(input("enter n: "))
+for i in range(1,n+1):
+    for j in range(1,n-i+1):
+        print(" ",end="")
+    for j in range(1,2*i):
+        if j==1 or i==1 or i==n:
+            print(j,end="")
+        elif j==2*i-1:
+            print("2",end="")
+        else:
+            print(" ",end="")
     print()
